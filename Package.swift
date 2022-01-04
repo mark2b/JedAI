1f37fd1ab9b4cbc2576a31da4d001f13faef058a
Mark Berner <mark2b@gmail.com>
2022-01-04 19:59:32 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -29,7 +29,7 @@ let package = Package(
 //        .binaryTarget(name: "JedAILambdaKit", path: "JedAILambdaKit.xcframework"),
 //        .binaryTarget(name: "JedAIJEMAKit", path: "JedAIJEMAKit.xcframework"),
 //        .binaryTarget(name: "JedAITripKit", path: "JedAITripKit.xcframework"),
-        .binaryTarget(name: "FMDB", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/xcframeworks/5.16.1/5.16.1.495/JedAIKit.xcframework.zip", checksum: "ccfb4b219c218068b0ca7bcf702aad832417fa93e7824cacba54573ce1851297"),
+        .binaryTarget(name: "FMDB", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/xcframeworks/5.16.1/5.16.1.495/FMDB.xcframework.zip", checksum: "ccfb4b219c218068b0ca7bcf702aad832417fa93e7824cacba54573ce1851297"),
 //        .binaryTarget(name: "SwiftProtobuf", path: "SwiftProtobuf.xcframework"),
 //        .binaryTarget(name: "ZipArchive", path: "ZipArchive.xcframework"),
 //        .binaryTarget(name: "Bugsnag", path: "Bugsnag.xcframework"),
