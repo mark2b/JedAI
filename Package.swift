e5ff8973c9625b177d53081bd1718b19336c520f
Mark Berner <mark2b@gmail.com>
2022-02-21 18:05:36 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -46,17 +46,6 @@ let package = Package(
             ]
         ),
         .library(
-            name: "JedAIKit",
-            targets: [
-                "JedAIKit",
-                "FMDB",
-                "SwiftProtobuf",
-                "ZipArchive",
-                "Bugsnag",
-            ]
-            
-        ),
-        .library(
             name: "JedAIConfigKit",
             targets: [
                 "JedAIConfigKit",
@@ -131,7 +120,6 @@ let package = Package(
         .binaryTarget(name: "JedAIScheduleKit", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/JedAIScheduleKit.xcframework.zip", checksum: "41b9e24414f1e9742bc0ca87cc1d6a3ab6a4e7236091e1142eb92fa9aa52d3f0"),
         .binaryTarget(name: "JedAITimelineKit", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/JedAITimelineKit.xcframework.zip", checksum: "c49aed88028eeca99775cc38e0a895a0cf582b45a3e0c44240a5827acfcb7fa2"),
         .binaryTarget(name: "FMDB", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/FMDB.xcframework.zip", checksum: "7d53af1452178356521af8ca51c95394bd5fe1001154f7ddb58497d1b10cbe98"),
-        .binaryTarget(name: "SwiftProtobuf", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/SwiftProtobuf.xcframework.zip", checksum: "9cedc6c2c30161641a2439e0be8bca9e75db834bc99d2aef89912947c729a219"),
         .binaryTarget(name: "ZipArchive", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/ZipArchive.xcframework.zip", checksum: "f2e37a2f1163bc405a08113ec68caa139b9cdf011d01eb965129a71ae0680d87"),
         .binaryTarget(name: "Bugsnag", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/Bugsnag.xcframework.zip", checksum: "6f3ea4bba8b844446d7c03f67043d981207211d051d595bd073c838876c9c1be"),
         .binaryTarget(name: "CocoaAsyncSocket", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/CocoaAsyncSocket.xcframework.zip", checksum: "23ffa0bb7cd4564d72a5f4581a012cb0f20c81fd95bb8f6411561d8785e9826b"),
