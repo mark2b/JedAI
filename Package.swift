a7f75d8960d141575b05e977bee9906dd889b005
Mark Berner <mark2b@gmail.com>
2022-02-22 05:54:18 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -90,15 +90,35 @@ let package = Package(
             url: "https://github.com/apple/swift-protobuf.git", "1.15.0" ..< "2.0.0"
         ),
         .package(name: "FMDB", url: "https://github.com/ccgus/fmdb", "2.7.0" ..< "2.8.0"),
-
+        .package(name: "ZipArchive", url: "https://github.com/ZipArchive/ZipArchive", "2.4.0" ..< "2.5.0"),
+        .package(name: "Bugsnag", url: "https://github.com/bugsnag/bugsnag-cocoa", "6.16.0" ..< "6.17.0"),
+        .package(name: "CocoaMQTT", url: "https://github.com/emqx/CocoaMQTT", "2.0.3-beta" ..< "2.1.0"),
+        .package(name: "Swifter", url: "https://github.com/httpswift/swifter", "1.5.0" ..< "1.6.0"),
     ],
     targets: [
         .target(
-            name: "swift-protobuf-dependency",
-            dependencies: ["SwiftProtobuf"]),
+            name: "SwiftProtobuf-dependency",
+            dependencies: ["SwiftProtobuf"]
+        ),
         .target(
-            name: "fmdb-dependency",
+            name: "FMDB-dependency",
             dependencies: ["FMDB"]
+        ),
+        .target(
+            name: "ZipArchive-dependency",
+            dependencies: ["ZipArchive"]
+        ),
+        .target(
+            name: "Bugsnag-dependency",
+            dependencies: ["Bugsnag"]
+        ),
+        .target(
+            name: "CocoaMQTT-dependency",
+            dependencies: ["CocoaMQTT"]
+        ),
+        .target(
+            name: "Swifter-dependency",
+            dependencies: ["Swifter"]
         ),
         .binaryTarget(name: "JedAIKit", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/JedAIKit.xcframework.zip", checksum: "d37598cd98b119679744c5a49c8d40ed182fe0d650a1adfe9871b363dc3ae226"),
         .binaryTarget(name: "JedAIUIKit", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/JedAIUIKit.xcframework.zip", checksum: "f0c3d6f1c4a075cb5cc228b3dad6080bb897b93d8cacd2e22272cc206d52ee56"),
@@ -114,12 +134,6 @@ let package = Package(
         .binaryTarget(name: "JedAIProfileKit", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/JedAIProfileKit.xcframework.zip", checksum: "b846a2da58978ccd07e873f9c7a0b479987a98b70ceced581b52d3a7b94473c0"),
         .binaryTarget(name: "JedAIScheduleKit", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/JedAIScheduleKit.xcframework.zip", checksum: "41b9e24414f1e9742bc0ca87cc1d6a3ab6a4e7236091e1142eb92fa9aa52d3f0"),
         .binaryTarget(name: "JedAITimelineKit", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/JedAITimelineKit.xcframework.zip", checksum: "c49aed88028eeca99775cc38e0a895a0cf582b45a3e0c44240a5827acfcb7fa2"),
-        .binaryTarget(name: "ZipArchive", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/ZipArchive.xcframework.zip", checksum: "f2e37a2f1163bc405a08113ec68caa139b9cdf011d01eb965129a71ae0680d87"),
-        .binaryTarget(name: "Bugsnag", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/Bugsnag.xcframework.zip", checksum: "6f3ea4bba8b844446d7c03f67043d981207211d051d595bd073c838876c9c1be"),
-        .binaryTarget(name: "CocoaAsyncSocket", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/CocoaAsyncSocket.xcframework.zip", checksum: "23ffa0bb7cd4564d72a5f4581a012cb0f20c81fd95bb8f6411561d8785e9826b"),
-        .binaryTarget(name: "CocoaMQTT", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/CocoaMQTT.xcframework.zip", checksum: "3c1ff7598987787af1621d698872139dbd00dc2cc45d83a287f53e4b8a8adea9"),
-        .binaryTarget(name: "Starscream", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/Starscream.xcframework.zip", checksum: "6c75f9320fbc36096602c4aca133630c6bb999917c6ac43921c2f8a16c8b5451"),
         .binaryTarget(name: "JedAISIMKit", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/JedAISIMKit.xcframework.zip", checksum: "819447d64981d488bc650a389b1d61acd8223920cb7b01834f29842a7e2a3926"),
-        .binaryTarget(name: "Swifter", url: "https://anagog-jedai-downloads.s3.amazonaws.com/ios/spm/0.17.0/0.17.0.541/Swifter.xcframework.zip", checksum: "cbb4645e12f79b7833f2e2d452409c0f25b871b1b5e593d8719266025c3104c3"),
     ]
 )
